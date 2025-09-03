f2745581c0f55ead3de40681ebcf3fa4d1a30d9a
joaoclemos <joaoclemos22@gmail.com>
2025-09-03 16:16:22 -0300

    Q1

---
+++ pratica1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    char letra1 = 'P';
+    char letra2 = 'D';
+    char letra3 = 'S';
+    char letra4 = '-';
+    char letra5 = '1';
+
+    printf("%d %d %d %d %d", letra1, letra2, letra3, letra4, letra5);
+
+
+    return 0;
+}
